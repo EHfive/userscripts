d65335087f74a703fdcd5a610f23fb2ed203b8af
Huang-Huang Bao <eh5@sokka.cn>
2020-02-23 00:02:22 +0800

    feat: dist files

---
+++ userscripts/bilibili-favorite-retrieval/dist/index.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name    bilibili-favorite-retrieval
+// @version 0.1.0
+// @include /^https?://space.bilibili.com/[0-9]+/favlist.*$/
+// @grant   unsafeWindow
+// @grant   GM_info
+// @grant   GM.info
+// @grant   GM_xmlhttpRequest
+// @grant   GM.xmlhttpRequest
+// ==/UserScript==
