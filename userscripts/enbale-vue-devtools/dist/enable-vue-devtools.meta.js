37eba42e3441537a3b36cbd2ee60c08aad978f5d
Huang-Huang Bao <eh5@sokka.cn>
2020-02-27 19:59:21 +0800

    [userscripts/enable-vue-devtools] release: 0.1.1

--- userscripts/enbale-vue-devtools/dist/enable-vue-devtools.meta.js
+++ userscripts/enbale-vue-devtools/dist/enable-vue-devtools.meta.js
@@ -1,13 +1,17 @@
 // ==UserScript==
-// @name        enable-vue-devtools
-// @version     0.1.0
-// @author      Huang-Huang Bao <eh5@sokka.cn> (https://github.com/EHfive)
-// @description Force enable Vue Devtools for a Vue production build.
-// @homepage    https://github.com/EHfive/userscripts/tree/master/userscripts/enbale-vue-devtools
-// @supportURL  https://github.com/EHfive/userscripts/issues
-// @run-at      document-start
-// @include     /^.*$/
-// @grant       unsafeWindow
-// @grant       GM_info
-// @grant       GM.info
+// @name              enable-vue-devtools
+// @version           0.1.1
+// @author            Huang-Huang Bao <eh5@sokka.cn> (https://github.com/EHfive)
+// @description       Force enable Vue Devtools for a Vue production build.
+// @homepage          https://github.com/EHfive/userscripts/tree/master/userscripts/enbale-vue-devtools
+// @supportURL        https://github.com/EHfive/userscripts/issues
+// @run-at            document-start
+// @name:zh-CN        强制开启VueDevtools
+// @name:zh-TW        強制開啓VueDevtools
+// @description:zh-CN 为生产构建的Vue应用强制开启VueDevtools
+// @description:zh-TW 爲生產構建的Vue應用強制開啓VueDevtools
+// @include           /^.*$/
+// @grant             unsafeWindow
+// @grant             GM_info
+// @grant             GM.info
 // ==/UserScript==
--- userscripts/enbale-vue-devtools/header.js
+++ userscripts/enbale-vue-devtools/header.js
@@ -1,10 +1,13 @@
 const isDev = require('./utils').isDev
 
 module.exports = function (data) {
-  const {author} = data
   return {
     version: isDev ? `[version]-build.[buildTime].no[buildNo]` : `[version]`,
     'run-at': 'document-start',
+    'name:zh-CN': '强制开启VueDevtools',
+    'name:zh-TW': '強制開啓VueDevtools',
+    'description:zh-CN': '为生产构建的Vue应用强制开启VueDevtools',
+    'description:zh-TW': '爲生產構建的Vue應用強制開啓VueDevtools',
     include: [
       /^.*$/
     ].map(reg => reg.toString()),
