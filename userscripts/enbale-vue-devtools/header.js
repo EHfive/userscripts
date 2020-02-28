81f84443e991ef5706563eaea6d18407163af8f2
Huang-Huang Bao <eh5@sokka.cn>
2020-02-29 01:36:47 +0800

    [userscripts/enable-vue-devtools] feat: update userscript header

--- userscripts/enbale-vue-devtools/header.js
+++ userscripts/enbale-vue-devtools/header.js
@@ -2,12 +2,13 @@ const isDev = require('./utils').isDev
 
 module.exports = function (data) {
   return {
+    'name': 'Force Enable Vue Devtools',
+    'name:zh-CN': '强制开启Vue Devtools',
+    'name:zh-TW': '強制開啓Vue Devtools',
+    'description:zh-CN': '为生产构建的Vue应用强制开启Vue Devtools',
+    'description:zh-TW': '爲生產構建的Vue應用強制開啓Vue Devtools',
     version: isDev ? `[version]-build.[buildTime].no[buildNo]` : `[version]`,
     'run-at': 'document-start',
-    'name:zh-CN': '强制开启VueDevtools',
-    'name:zh-TW': '強制開啓VueDevtools',
-    'description:zh-CN': '为生产构建的Vue应用强制开启VueDevtools',
-    'description:zh-TW': '爲生產構建的Vue應用強制開啓VueDevtools',
     include: [
       /^.*$/
     ].map(reg => reg.toString()),
