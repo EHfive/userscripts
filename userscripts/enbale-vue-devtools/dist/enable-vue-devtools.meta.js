d4582a4ded6694423f460c63dbd1437e8fdc7a8a
Huang-Huang Bao <eh5@sokka.cn>
2020-02-29 03:26:19 +0800

    [userscripts/enable-vue-devtools] release: 0.1.2

--- userscripts/enbale-vue-devtools/dist/enable-vue-devtools.meta.js
+++ userscripts/enbale-vue-devtools/dist/enable-vue-devtools.meta.js
@@ -1,15 +1,15 @@
 // ==UserScript==
-// @name              enable-vue-devtools
+// @name              Force Enable Vue Devtools
 // @version           0.1.1
 // @author            Huang-Huang Bao <eh5@sokka.cn> (https://github.com/EHfive)
 // @description       Force enable Vue Devtools for a Vue production build.
 // @homepage          https://github.com/EHfive/userscripts/tree/master/userscripts/enbale-vue-devtools
 // @supportURL        https://github.com/EHfive/userscripts/issues
+// @name:zh-CN        强制开启Vue Devtools
+// @name:zh-TW        強制開啓Vue Devtools
+// @description:zh-CN 为生产构建的Vue应用强制开启Vue Devtools
+// @description:zh-TW 爲生產構建的Vue應用強制開啓Vue Devtools
 // @run-at            document-start
-// @name:zh-CN        强制开启VueDevtools
-// @name:zh-TW        強制開啓VueDevtools
-// @description:zh-CN 为生产构建的Vue应用强制开启VueDevtools
-// @description:zh-TW 爲生產構建的Vue應用強制開啓VueDevtools
 // @include           /^.*$/
 // @grant             unsafeWindow
 // @grant             GM_info
