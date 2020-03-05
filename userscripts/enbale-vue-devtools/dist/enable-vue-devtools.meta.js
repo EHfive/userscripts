62cde69da69da6e8aafdfaefe086fd157b33f144
Huang-Huang Bao <eh5@sokka.cn>
2020-03-06 03:48:01 +0800

    [userscripts/enable-vue-devtools] release: 0.1.5

--- userscripts/enbale-vue-devtools/dist/enable-vue-devtools.meta.js
+++ userscripts/enbale-vue-devtools/dist/enable-vue-devtools.meta.js
@@ -1,15 +1,18 @@
 // ==UserScript==
 // @name              Force Enable Vue Devtools
-// @version           0.1.4
+// @version           0.1.5
 // @author            Huang-Huang Bao <eh5@sokka.cn> (https://github.com/EHfive)
 // @description       Force enable Vue Devtools for a Vue production build.
 // @homepage          https://github.com/EHfive/userscripts/tree/master/userscripts/enbale-vue-devtools
 // @supportURL        https://github.com/EHfive/userscripts/issues
+// @namespace         https://eh5.me
 // @name:zh-CN        强制开启Vue Devtools
 // @name:zh-TW        強制開啓Vue Devtools
 // @description:zh-CN 为生产构建的Vue应用强制开启Vue Devtools
 // @description:zh-TW 爲生產構建的Vue應用強制開啓Vue Devtools
 // @license           MIT
+// @updateURL         https://github.com/EHfive/userscripts/raw/master/userscripts/enbale-vue-devtools/dist/enable-vue-devtools.meta.js
+// @downloadURL       https://github.com/EHfive/userscripts/raw/master/userscripts/enbale-vue-devtools/dist/enable-vue-devtools.user.js
 // @run-at            document-start
 // @include           /^.*$/
 // @grant             unsafeWindow
--- userscripts/enbale-vue-devtools/header.js
+++ userscripts/enbale-vue-devtools/header.js
@@ -3,6 +3,7 @@ const pkg = require('./package')
 
 module.exports = function (data) {
   return {
+    namespace: 'https://eh5.me',
     name: 'Force Enable Vue Devtools',
     'name:zh-CN': '强制开启Vue Devtools',
     'name:zh-TW': '強制開啓Vue Devtools',
@@ -10,6 +11,8 @@ module.exports = function (data) {
     'description:zh-TW': '爲生產構建的Vue應用強制開啓Vue Devtools',
     license: pkg.license,
     version: isDev ? `[version]-build.[buildTime].no[buildNo]` : `[version]`,
+    updateURL: 'https://github.com/EHfive/userscripts/raw/master/userscripts/enbale-vue-devtools/dist/enable-vue-devtools.meta.js',
+    downloadURL: 'https://github.com/EHfive/userscripts/raw/master/userscripts/enbale-vue-devtools/dist/enable-vue-devtools.user.js',
     'run-at': 'document-start',
     include: [
       /^.*$/
