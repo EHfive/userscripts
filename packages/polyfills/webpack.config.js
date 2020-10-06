70fa732982c8582a2a3f5a88b69aa77d7969cdbf
Huang-Huang Bao <eh5@sokka.cn>
2020-10-07 01:27:11 +0800

    dep: upgrade

--- packages/polyfills/webpack.config.js
+++ packages/polyfills/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
         use: {
           loader: require.resolve('imports-loader'),
           options: {
-            this: '>window'
+            wrapper: 'window'
           }
         }
       }
